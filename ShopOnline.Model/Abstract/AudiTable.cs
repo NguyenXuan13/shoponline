cf952325cf68fa74821f0ede28d93e6ea0c82223
XuanNguyen13 <eragonzappy@gmail.com>
2018-05-02 13:59:20 +0700

    Xây dựng tâng Domain Enities cho dự án

---
+++ ShopOnline.Model/Abstract/AudiTable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShopOnline.Model.Models
+{
+    public abstract class AudiTable : IAudiTable
+    {
+        public DateTime? CreateDate { get; set; }
+
+        [MaxLength(256)]
+        public string CreatedBy { get; set; }
+
+        public DateTime? UpdatedDate { get; set; }
+
+        [MaxLength(256)]
+        public string UpdateBy { get; set; }
+
+        public bool Status { get; set; }
+
+        [MaxLength(256)]
+        public string MetaKeyword { get; set; }
+
+        [MaxLength(256)]
+        public string MetaDescription { get; set; }
+    }
+}
