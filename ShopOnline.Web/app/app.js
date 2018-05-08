9bc794a9843caace612acf92853089e30e4c6b49
XuanNguyen13 <eragonzappy@gmail.com>
2018-05-09 02:03:20 +0700

    Triển khai cơ chế routing cho SPA bằng angular ui router

--- ShopOnline.Web/app/app.js
+++ ShopOnline.Web/app/app.js
@@ -1,1 +1,16 @@
-﻿
+﻿/// <reference path="/Assets/Admin/libs/angular/angular.js" />
+
+(function () {
+    angular.module('shoponline', ['shoponline.products', 'shoponline.common']).config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('home', {
+            url: "/Admin",
+            templateUrl: "/app/components/home/homeView.html",
+            controller: "homeController"
+        });
+        $urlRouterProvider.otherwise('/Admin');
+    }
+})();
